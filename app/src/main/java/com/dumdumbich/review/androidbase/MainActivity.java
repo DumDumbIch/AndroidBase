ff22259fc6d9f5894222f65ffae72f04719b7463
DumDumbIch <dumdumbich@gmail.com>
2023-01-27 19:21:24 +0300

    Initial commit

---
+++ app/src/main/java/com/dumdumbich/review/androidbase/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dumdumbich.review.androidbase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
